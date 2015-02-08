fa6d110f2220f8fd7c9fee218c4e4e8100025f35
petersme <mpetersen389@gmail.com>
2015-02-08 00:57:06 -0500

    edited plot and main
    Added plot.js reference to main.html, added basic Flot code to plot.js.

---
+++ plot.js
@@ -0,0 +1,27 @@
+
+
+
+var plot = $.plot("#placeholder", [ getRandomData() ], {
+			series: {
+				shadowSize: 0	// Drawing is faster without shadows
+			},
+			yaxis: {
+				min: 0,
+				max: 100
+			},
+			xaxis: {
+				show: false
+			}
+		});
+
+		function update() {
+
+			plot.setData([getRandomData()]);
+
+			// Since the axes don't change, we don't need to call plot.setupGrid()
+
+			plot.draw();
+			setTimeout(update, updateInterval);
+		}
+
+		update();
